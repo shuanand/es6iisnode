867143e0faa1740153ff33f52b8e0bfddfc2ade1
shuanand <shuanand@microsoft.com>
2022-05-16 20:45:35 +0530

    second commit

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ import express from 'express';
 const app = express();
   
 app.get('/',(req,res) => {
-    res.send('test');
+const body = "testdata"
+// Adds header
+res.append('Content-Type', 'text/plain;charset=utf-8');
+res.append('Connection', 'keep-alive')
+res.append('Set-Cookie', 'divehours=fornightly')
+res.append('Content-Length', Buffer.byteLength(body, 'utf-8'));
+res.status(200).send(body);
 })
   
 const PORT = process.env.PORT||3000;
